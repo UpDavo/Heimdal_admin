6543658d8b06101418dc9f77fb58f7ee00de9ca3
Updavo <updavo@heimdalec.com>
2022-06-22 09:32:44 -0500

    map

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root",
 })
 export class ApiService {
-  private url = "http://localhost:3000/api/pocs?quantity=all";
+  private url = "https://heimdalec-ws2.herokuapp.com/api/pocs?quantity=all";
 
   constructor(private httpClient: HttpClient) {}
 
