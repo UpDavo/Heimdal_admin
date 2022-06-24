6f0f4705dbbd200052625ca630ecd993394e90bc
Daniela Medina <dalmeri@iMac-de-Daniela.local>
2022-06-24 13:52:52 -0500

    nueva tabla en el dashboard

--- src/app/components/cards/card-social-traffic/card-social-traffic.component.ts
+++ src/app/components/cards/card-social-traffic/card-social-traffic.component.ts
@@ -1,11 +1,54 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
   selector: "app-card-social-traffic",
   templateUrl: "./card-social-traffic.component.html",
 })
 export class CardSocialTrafficComponent implements OnInit {
+  @Input() sales;
+  errors = {
+    "Banco no aceptó la transacción": 0,
+    "Cliente no respondió": 0,
+    "Datos de tarjeta incorrectos": 0,
+    "Fondos Insuficientes": 0,
+    "No fue posible validar el pago, por favor vuelva a intentarlo con otros datos.": 0,
+    "Tarjeta temporalmente inactivada": 0,
+    "Transacción negada": 0,
+  };
+  percentages = [];
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.sales.forEach((client) => {
+      this.errors["Banco no aceptó la transacción"] +=
+        client.errors["Banco no aceptó la transacción"];
+      this.errors["Cliente no respondió"] +=
+        client.errors["Cliente no respondió"];
+      this.errors["Datos de tarjeta incorrectos"] +=
+        client.errors["Datos de tarjeta incorrectos"];
+      this.errors["Fondos Insuficientes"] +=
+        client.errors["Fondos Insuficientes"];
+      this.errors[
+        "No fue posible validar el pago, por favor vuelva a intentarlo con otros datos."
+      ] +=
+        client.errors[
+          "No fue posible validar el pago, por favor vuelva a intentarlo con otros datos."
+        ];
+      this.errors["Tarjeta temporalmente inactivada"] +=
+        client.errors["Tarjeta temporalmente inactivada"];
+      this.errors["Transacción negada"] += client.errors["Transacción negada"];
+    });
+    this.percentages = this.getPercentages();
+  }
+
+  getPercentages() {
+    let array = [];
+    for (const property in this.errors) {
+      array.push(this.errors[property]);
+    }
+    const sum = array.reduce((partialSum, a) => partialSum + a, 0);
+    return array.map((a) => {
+      return (a * 100) / sum;
+    });
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,8 +5,8 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root",
 })
 export class ApiService {
-  //private url = "https://heimdalec-ws2.herokuapp.com/api/pocs?quantity=all";
-  private url = "https://heimdalec-ws.herokuapp.com";
+  private url = "http://localhost:3000";
+  //private url = "https://heimdalec-ws.herokuapp.com";
 
   constructor(private httpClient: HttpClient) {}
 
